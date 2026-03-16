b258b96e9d3996150ee46baba7340021782eda1e
gowthamgowtham0912 <gowthamgowtham0912@gmail.com>
2026-03-16 09:19:20 +0530

    planidromecheckerapp

---
+++ app/src/PalinodromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalinodromeCheckerApp {
+    static void main() {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("system initialized successfully");
+
+    }
+}
